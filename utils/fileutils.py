45dfc8564c609f70fed89139e32607c88cb50bee
Clément Boittin <clement.boittin@protonmail.com>
2022-05-20 20:14:56 +0200

    ✨ Update load_image util

--- utils/fileutils.py
+++ utils/fileutils.py
@@ -8,14 +8,17 @@ def load_image(path, colorkey=None):
     fullname = os.path.join('assets/images', path)
     try:
         image = pygame.image.load(fullname)
+        if image.get_alpha() is None:
+            image = image.convert()
+            if colorkey is not None:
+                if colorkey is -1:
+                    colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey, RLEACCEL)
+        else:
+            image = image.convert_alpha()
     except pygame.error as error:
         print("Impossible de charger l'image :", path)
         raise SystemExit(error.message)
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, RLEACCEL)
     return image, image.get_rect()
 
 
