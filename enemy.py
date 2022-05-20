746a1b2ff9964e2e1341cdb75859200127d3d157
Doyesap <doyesap@protonmail.com>
2022-05-20 20:28:20 +0200

    Test render enemy (not working)

--- enemy.py
+++ enemy.py
@@ -1,11 +1,16 @@
+import pygame
+from pygame.locals import *
+from utils import fileutils
+
 class Enemy(pygame.sprite.Sprite): 
     """Enemy class"""
     def __init__(self, start_x, start_y):
         pygame.sprite.Sprite.__init__(self)        #Appel du constructeur de Sprite
-        self.coordinates = (star_x, start_y)
-        self.image, self.rect = load_image('chimp.bmp', -1)
+        self.coordinates = (start_x, start_y)
+        self.image, self.rect = fileutils.load_image('testsquare.png', -1)
         screen = pygame.display.get_surface()
         
 
     def update(self):
+        pass
         # Update
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 import sys
 import pygame
 from pygame.locals import *
+from enemy import Enemy
 
 if not pygame.font:
     print('Attention, polices désactivées')
 if not pygame.mixer:
     print('Attention, son désactivé')
 
+enemies = []
 
 def main():
     # Initialisation de pygame
@@ -37,10 +39,14 @@ def main():
         main_sprites.draw(screen)
         pygame.display.flip()
 
+    create_enemy(200, 200)
+
     while running:
         clock.tick(60)
         render()
 
+def create_enemy(start_x, start_y):
+    enemies.append(Enemy(start_x, start_y))
 
 if __name__ == '__main__':
     main()
