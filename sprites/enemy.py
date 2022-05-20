9c9407fa5589aa81f984c040c8eb7bc93055964b
Doyesap <doyesap@protonmail.com>
2022-05-21 01:49:46 +0200

    support different enemies

--- sprites/enemy.py
+++ sprites/enemy.py
@@ -6,19 +6,22 @@ from utils import screenutils
 
 class Enemy(pygame.sprite.Sprite): 
     """Enemy class"""
-    def __init__(self, level_manager, start_x, start_y):
+    def __init__(self, level_manager, start_x, start_y, enemy_type):
         pygame.sprite.Sprite.__init__(self)        #Appel du constructeur de Sprite
         self.level_manager = level_manager
+        self.enemy_type = enemy_type
         self.coordinates = (start_x, start_y)
-        self.image, self.rect = fileutils.load_image('enemy.png')
+        if self.enemy_type == 0:
+            self.image, self.rect = fileutils.load_image('enemy.png')
         self.rect.topleft = self.coordinates
         screen = pygame.display.get_surface()
         
 
     def update(self):
-        self.coordinates = (self.coordinates[0], self.coordinates[1] + 2)
-        self.rect.topleft = self.coordinates
-        if self.coordinates[1] > screenutils.get_heigth():
-            self.level_manager.on_enemy_deleted()
-            self.kill()
+        if self.enemy_type == 0:
+            self.coordinates = (self.coordinates[0], self.coordinates[1] + 2)
+            self.rect.topleft = self.coordinates
+            if self.coordinates[1] > screenutils.get_heigth():
+                self.level_manager.on_enemy_deleted()
+                self.kill()
         # Update
