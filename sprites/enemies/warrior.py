610cfd447e1cac15511a73ff96b4142e3b552ebb
Clément Boittin <clement.boittin@protonmail.com>
2022-05-21 06:30:53 +0200

    First warrior

---
+++ sprites/enemies/warrior.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+from sprites.enemy import Enemy
+
+
+class Warrior(Enemy):
+    def __init__(self, game, level_manager, pos, tier):
+        assert tier in [1, 2, 3]
+        options = {}
+        options["image"] = f"warrior${tier}.png"
+        if tier == 1:
+            options["life"] = 1
+
+            def on_update(self):
+                self.rect.top += 1
+        Enemy.__init__(game, level_manager, pos, options)
