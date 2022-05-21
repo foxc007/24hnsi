437abde1ff2b16acec23e1e2bfb4adaf6d7cdf76
Clément Boittin <clement.boittin@protonmail.com>
2022-05-21 04:30:24 +0200

    Change bullet speed

--- sprites/bullet.py
+++ sprites/bullet.py
@@ -12,16 +12,17 @@ class Bullet(pygame.sprite.Sprite):
         self.level_manager = level_manager
         self.bullet_type = bullet_type
         if self.bullet_type == 0:
-            self.image, self.rect = fileutils.load_image('bullet.jpg')
+            self.image, self.rect = fileutils.load_image(
+                'bullet.jpg', colorkey=-1)
             self.rect.size = (50, 50)
             self.image = pygame.transform.scale(
                 self.image, (self.rect.width, self.rect.height))
         self.rect.centerx, self.rect.bottom = start_x, start_y+30
-        screen = pygame.display.get_surface() 
+        screen = pygame.display.get_surface()
 
     def update(self):
         if self.bullet_type == 0:
-            self.rect.top -= 25
+            self.rect.top -= 10
             if self.rect.top < 0:
                 self.kill()
         # Update
