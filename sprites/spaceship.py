4f1d838f9377952a47f659083273d3b96db5c14a
Clément Boittin <clement.boittin@protonmail.com>
2022-05-21 05:18:35 +0200

    Enhancements

--- sprites/spaceship.py
+++ sprites/spaceship.py
@@ -13,32 +13,34 @@ class Spaceship(pygame.sprite.Sprite):
         self.screen_size = screen.get_size()
         self.rect.centerx = self.screen_size[0] / 2
         self.rect.y = self.screen_size[1]*5//6
-        self.speed = 10
+        self.base_speed = 10
+        self.base_reload_time = 1000
         self.last_shot_time = 0
 
     def update(self, keys_pressed: list):
-        self.time_between_shots = 1000
+        self.speed = self.base_speed*self.game.speed_coef
+        self.reload_time = self.base_reload_time/self.game.speed_coef
         if not (K_LEFT in keys_pressed and K_RIGHT in keys_pressed):
             if K_LEFT in keys_pressed:
                 self.move_left()
             elif K_RIGHT in keys_pressed:
                 self.move_right()
-        if K_SPACE in keys_pressed and time.time() - self.last_shot_time > self.time_between_shots / 1000:
+        if K_SPACE in keys_pressed and time.time() - self.last_shot_time > self.reload_time / 1000:
             self.last_shot_time = time.time()
             self.shoot()
 
     def move_right(self):
-        if self.rect.right + self.speed > self.screen_size[0]:
-            self.rect.right = self.screen_size[0]
+        if self.rect.centerx + self.speed > self.screen_size[0]:
+            self.rect.left = 0
         else:
             self.rect.right += self.speed
 
     def move_left(self):
-        if self.rect.left - self.speed < 0:
-            self.rect.left = 0
+        if self.rect.centerx - self.speed < 0:
+            self.rect.right = self.screen_size[0]
         else:
             self.rect.left -= self.speed
 
     def shoot(self):
         self.game.bullet_sprites.add(bullet.Bullet(self.game,
-                                                  self.rect.centerx, self.rect.top, 0))
+                                                   self.rect.centerx, self.rect.top, 0))
