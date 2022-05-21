72e4ce51c451e46391f6edc254921b34a2d1c9a3
Clément Boittin <clement.boittin@protonmail.com>
2022-05-21 06:17:12 +0200

    New enemy class

--- sprites/enemy.py
+++ sprites/enemy.py
@@ -5,30 +5,32 @@ import level_manager
 from utils import screenutils
 from random import randint
 
-class Enemy(pygame.sprite.Sprite): 
-    """Enemy class"""
-    def __init__(self, level_manager, start_x, start_y, enemy_type, game):
-        self.game = game
-        pygame.sprite.Sprite.__init__(self)        #Appel du constructeur de Sprite
-        self.level_manager = level_manager
-        self.enemy_type = enemy_type
-        self.coordinates = (start_x, start_y)
-        if self.enemy_type == 0:
-            self.image, self.rect = fileutils.load_image('enemy.png')
-        self.rect.topleft = self.coordinates
-        screen = pygame.display.get_surface()
-        
-    def delete(self):
-            self.level_manager.on_enemy_deleted()
-            self.kill()
-            pygame.mixer.Sound.play(fileutils.load_sound(f'alien_death{randint(1,5)}.ogg'))
 
+class Enemy(pygame.sprite.Sprite):
+    """Enemy class"""
+
+    def __init__(self, game, level_manager, pos, options: dict):
+        pygame.sprite.Sprite.__init__(self)  # Appel du constructeur de Sprite
+        self.game = game
+        self.level_manager = level_manager
+        self.on_update = options["on_update"]
+        self.image, self.rect = fileutils.load_image(options["image"])
+        self.life = options["life"]
+        if options["size"] is not None:
+            self.rect.size = options["size"]
+        else:
+            self.rect.size = (64, 64)
+        self.rect.center = pos
+
+    def delete(self):
+        self.level_manager.on_enemy_deleted()
+        self.kill()
+        pygame.mixer.Sound.play(self.game.alien_death_sounds[randint(
+            0, len(self.game.alien_death_sounds)-1)])
 
     def update(self):
-        if self.enemy_type == 0:
-            self.coordinates = (self.coordinates[0], self.coordinates[1] + 2 * self.game.speed_coef)
-            self.rect.topleft = self.coordinates
-            if self.coordinates[1] > screenutils.get_heigth():
-                self.level_manager.on_enemy_deleted()
-                self.kill()
+        self.on_update(self)
+        if self.rect.bottom > screenutils.get_heigth():
+            self.level_manager.on_enemy_deleted()
+            self.kill()
         # Update
