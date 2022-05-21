8b49ae854b43761b213b0ce70b00a62e51b61e5c
Clément Boittin <clement.boittin@protonmail.com>
2022-05-21 10:41:51 +0200

    fix

--- level_manager.py
+++ level_manager.py
@@ -18,7 +18,7 @@ class LevelManager:
         pass
 
     def create_enemy(self):
-        x = random.randint(400, screenutils.get_width()-400)
+        x = random.randint(500, screenutils.get_width()-500)
         y = screenutils.get_heigth() * 0.05
         self.game.enemy_sprites.add(
             Striker(self.game, self, (x, y), 1))
